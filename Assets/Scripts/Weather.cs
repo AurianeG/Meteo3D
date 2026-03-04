5fbf1800d41e7706ef323fbd87f89134dd45ca9f
AurianeG <auriane.gonindard@gmail.com>
2026-03-04 09:43:49 -0500

    ajout des differents codes meteo dans le script

--- Assets/Scripts/Weather.cs
+++ Assets/Scripts/Weather.cs
@@ -6,7 +6,22 @@ public class SimpleWeather : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI temperatureText;
     public GameObject sun;
+    public GameObject sunAudio;
+
     public GameObject rain;
+    public GameObject rainAudio;
+
+    public GameObject snow;
+    public GameObject snowAudio;
+
+    public GameObject fog;
+    public GameObject fogAudio;
+
+    public GameObject cloud;
+    public GameObject cloudAudio;
+
+    public GameObject thunder;
+    public GameObject thunderAudio;
     
 
     void Start()
@@ -27,8 +42,12 @@ public class SimpleWeather : MonoBehaviour
             int code = data.current_weather.weathercode;
             temperatureText.text = temp + "°C";
             // Activation simple selon le code
-            rain.SetActive(code >= 61 && code <= 67);
-            sun.SetActive(code <= 1);
+            sun.SetActive(code <= 2);
+            rain.SetActive((code >= 51 && code <= 67) || (code >= 80 && code <= 82));
+            snow.SetActive((code >= 71 && code <= 77) || (code >= 85 && code <= 86));
+            fog.SetActive(code >= 45 && code <= 48);
+            cloud.SetActive(code == 3 || (code >= 20 && code <= 40));
+            thunder.SetActive(code >= 95 && code <= 99);
         }
         else
         {
